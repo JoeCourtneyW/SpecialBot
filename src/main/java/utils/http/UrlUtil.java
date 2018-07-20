472058c82878e24acda61cb5faad94cf528ad6d5
Joe Courtney <courtneyjoew@gmail.com>
2018-07-20 10:28:36 -0400

    Compile youtube regex pattern instead of using Pattern.matches

--- src/main/java/utils/http/UrlUtil.java
+++ src/main/java/utils/http/UrlUtil.java
@@ -8,6 +8,13 @@ import java.net.URLEncoder;
 import java.util.regex.Pattern;
 
 public class UrlUtil {
+
+    private static Pattern youtubeRegex;
+
+    static {
+        youtubeRegex = Pattern.compile("^(https?://)?(www\\.)?(youtube\\.com|youtu\\.?be)/.+$");
+    }
+
     public static boolean isUrl(String verify) {
         try {
             new URL(verify).toURI();
@@ -18,7 +25,7 @@ public class UrlUtil {
     }
 
     public static boolean isYoutubeURL(String url) {
-        return Pattern.matches("^(https?://)?(www\\.)?(youtube\\.com|youtu\\.?be)/.+$", url);
+        return youtubeRegex.matcher(url).matches();
     }
 
     public static URL getUrl(String url) {
@@ -35,11 +42,11 @@ public class UrlUtil {
         try {
             result = URLEncoder.encode(s, "UTF-8")
                     .replaceAll("\\+", "%20")
-                    .replaceAll("\\%21", "!")
-                    .replaceAll("\\%27", "'")
-                    .replaceAll("\\%28", "(")
-                    .replaceAll("\\%29", ")")
-                    .replaceAll("\\%7E", "~");
+                    .replaceAll("%21", "!")
+                    .replaceAll("%27", "'")
+                    .replaceAll("%28", "(")
+                    .replaceAll("%29", ")")
+                    .replaceAll("%7E", "~");
         } catch (UnsupportedEncodingException e) {
             result = s;
         }
